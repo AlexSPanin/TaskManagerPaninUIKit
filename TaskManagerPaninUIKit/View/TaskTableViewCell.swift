913f511ada815cf04503d3402cb337dd2b1bb7eb
Alexander Panin <panin1970@gmail.com>
2022-02-24 11:58:00 +0300

    add folders and new files

---
+++ TaskManagerPaninUIKit/View/TaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTableViewCell.swift
+//  TaskManagerPaninUIKit
+//
+//  Created by Александр Панин on 24.02.2022.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
